22cf5071fdab6a8129973220c8c64e7b9bbc76d3
JesusL <thedarkness2555@gmail.com>
2021-06-17 19:28:49 -0500

    Se crean y parametrizan las entidades del agregado

--- src/main/java/parking/Cliente.java
+++ src/main/java/parking/Cliente.java
@@ -1,4 +1,36 @@
 package parking;
 
-public class Cliente {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.domain.generic.Identity;
+import parking.values.ClienteId;
+import parking.values.CorreoCliente;
+import parking.values.NombreCliente;
+
+import java.util.Objects;
+
+public class Cliente extends Entity<ClienteId> {
+    private NombreCliente nombreCliente;
+    private CorreoCliente correoCliente;
+
+    public Cliente(ClienteId entityId, NombreCliente nombreCliente, CorreoCliente correoCliente) {
+        super(entityId);
+        this.nombreCliente = nombreCliente;
+        this.correoCliente = correoCliente;
+    }
+
+    public void ActualizarNombreCliente(NombreCliente nombreCliente){
+        this.nombreCliente = Objects.requireNonNull(nombreCliente);
+    }
+
+    public void ActualizarCorreoCliente(CorreoCliente correoCliente){
+        this.correoCliente = Objects.requireNonNull(correoCliente);
+    }
+
+    public NombreCliente NombreCliente() {
+        return nombreCliente;
+    }
+
+    public CorreoCliente CorreoCliente() {
+        return correoCliente;
+    }
 }
--- src/main/java/parking/Vehiculo.java
+++ src/main/java/parking/Vehiculo.java
@@ -1,4 +1,37 @@
 package parking;
 
-public class Vehiculo {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.domain.generic.Identity;
+import parking.values.NombrePropietario;
+import parking.values.TipoVehiculo;
+import parking.values.VehiculoId;
+
+import java.util.Objects;
+
+public class Vehiculo extends Entity<VehiculoId> {
+    private NombrePropietario nombrePropietario;
+    private TipoVehiculo tipoVehiculo;
+
+    public Vehiculo(VehiculoId entityId, NombrePropietario nombrePropietario, TipoVehiculo tipoVehiculo) {
+        super(entityId);
+        this.nombrePropietario = nombrePropietario;
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public void ActualizarNombrePropietario(NombrePropietario nombrePropietario){
+        this.nombrePropietario = Objects.requireNonNull(nombrePropietario);
+    }
+
+
+    public void ActualizarTipoVehiculo(TipoVehiculo tipoVehiculo){
+        this.tipoVehiculo = Objects.requireNonNull(tipoVehiculo);
+    }
+
+    public NombrePropietario NombrePropietario() {
+        return nombrePropietario;
+    }
+
+    public TipoVehiculo TipoVehiculo() {
+        return tipoVehiculo;
+    }
 }
